132ae7bdc92f6038c24d6d40108b215280613e5c
t-matsumo <tatsukimatsumo@gmail.com>
2020-01-04 12:28:31 +0900

    20190104

--- kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val table = readLine()!!
-    val hand = readLine()!!
-    var a = 11
-    a = 1 + 2
-    val ans = if (hand.contains(table[0]) || hand.contains(table[1])) "yes" else "no"
-    println(ans)
-}
---
+++ kotlin/src/main/kotlin/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println("aaaa")
+    val a = 1
+    println(a)
+}
